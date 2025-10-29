51b1ed5de34451d56091685dd21f58c1845660fc
StephanoGit <poporaduu@gmail.com>
2025-10-29 15:30:10 +0000

    edit: nix-darwin pakages and .zhsrc

---
+++ nix-darwin/nix-darwin/flake.nix
@@ -0,0 +1,132 @@
+{
+  description = "Stephano's Darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew, ... }:
+    let
+      configuration = { pkgs, config, ... }: {
+
+        system.primaryUser = "stephano.";
+
+        homebrew = {
+          enable = true;
+          casks = [
+
+          ];
+          onActivation.cleanup = "zap";
+          onActivation.autoUpdate = true;
+          onActivation.upgrade = true;
+        };
+
+        nixpkgs.config.allowUnfree = true;
+
+        # List packages installed in system profile. To search by name, run:
+        # $ nix-env -qaP | grep wget
+        environment.systemPackages = [
+          pkgs.iterm2
+          pkgs.neovim
+          pkgs.tmux
+          pkgs.oh-my-posh
+          pkgs.fzf
+          pkgs.zsh-fzf-tab
+          pkgs.mkalias
+          pkgs.skhd
+          pkgs.nodejs
+          pkgs.nixfmt-classic
+          pkgs.statix
+          pkgs.tree
+
+        ];
+
+        services.skhd = {
+          enable = true;
+          skhdConfig = ''
+            cmd - u : say "it works bro"
+
+            # When you press Shift + 3, send Option + 3 (produces #)
+            alt - 3 : skhd -t "£"
+
+            # When you press Option + 3, send Shift + 3 (produces £)
+            shift - 3 : skhd -t "#"
+          '';
+        };
+
+        # fonts.packages = [
+        #   (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+        # ];
+
+        system.activationScripts.applications.text = let
+          env = pkgs.buildEnv {
+            name = "system-applications";
+            paths = config.environment.systemPackages;
+            pathsToLink = "/Applications";
+          };
+        in pkgs.lib.mkForce ''
+          # Set up applications.
+          echo "setting up /Applications..." >&2
+          rm -rf /Applications/Nix\ Apps
+          mkdir -p /Applications/Nix\ Apps
+          find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
+          while read -r src; do
+            app_name=$(basename "$src")
+            echo "copying $src" >&2
+            ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
+          done
+        '';
+
+        system.defaults = {
+          finder.FXPreferredViewStyle = "clmv";
+
+        };
+
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
+
+        # Enable alternative shell support in nix-darwin.
+        programs.zsh.enable = true;
+        # programs.fish.enable = true;
+
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
+
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+      };
+    in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#Stephanos-MacBook-Pro-2
+      darwinConfigurations."m4-pro" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "stephano.";
+
+              autoMigrate = true;
+            };
+          }
+        ];
+      };
+
+      darwinPackages = self.darwinConfigurations."m4-pro".pkgs;
+    };
+}
